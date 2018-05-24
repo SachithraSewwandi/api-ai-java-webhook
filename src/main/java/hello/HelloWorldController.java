f05a2955a0f5b1e76111452cd4570a6699752640
sewwandiwi <sewwandisachithra@gmail.com>
2018-05-24 11:50:26 +0530

    commit 2

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -26,24 +26,22 @@ public class HelloWorldController {
         //Boolean word= obj.contains("intentName");
         String test2=test.substring(test.indexOf(":")+1, test.indexOf("}")).trim();
         String test3=test2.substring(1,test2.length()-1);
-        System.out.println(test);
-        System.out.println(test3);
+        //System.out.println(test);
+        //System.out.println(test3);
         String greeting="Welcome";
 
         if(test3.equalsIgnoreCase("Default Welcome Intent")){
 
-
-
             GregorianCalendar time = new GregorianCalendar();
             if(time.get(Calendar.HOUR_OF_DAY)<12){
-                greeting="Good Morning";
+                greeting="Good Morning !";
             }else if(time.get(Calendar.HOUR_OF_DAY)>=12 && time.get(Calendar.HOUR_OF_DAY)<16){
-                greeting="Good Afternoon";
+                greeting="Good Afternoon !";
             }else {
-                greeting="Good Evening";
+                greeting="Good Evening !";
             }
+            greeting=greeting+ ", Welcome to Mobitel Virtual Private Assistant. How Can I help You.";
         }
-
-        return new WebhookResponse( greeting +".! \n Welcome to Mobitel Virtual Private Assistant", "Text " );
+        return new WebhookResponse( greeting , "Text " );
     }
 }
