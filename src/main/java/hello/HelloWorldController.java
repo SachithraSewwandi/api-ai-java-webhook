3b185592f7f594c644a547484e300345cac772bc
Sachithra Sewwandi <sewwandiwi@mobitel.lk>
2018-06-01 22:20:23 +0530

    new request set

--- src/main/java/hello/HelloWorldController.java
+++ src/main/java/hello/HelloWorldController.java
@@ -22,15 +22,17 @@ public class HelloWorldController {
     public @ResponseBody WebhookResponse webhook(@RequestBody String obj){
 
         System.out.println(obj);
-        String test= obj.substring(obj.indexOf("intentName")+1, obj.indexOf("fulfillment"));
-        //Boolean word= obj.contains("intentName");
-        String test2=test.substring(test.indexOf(":")+1, test.indexOf("}")).trim();
-        String test3=test2.substring(1,test2.length()-1);
+        System.out.println("*******");
+        String test= obj.substring(obj.indexOf("intent"), obj.indexOf("intentDetectionConfidence"));
+        //Boolean word= obj.contains("intent");
+        String test2=test.substring(test.indexOf("displayName")+15, test.indexOf("}")).trim();
+        String test3=test2.substring(0,test2.length()-1);
         //System.out.println(test);
+        //System.out.println(test2);
         //System.out.println(test3);
         String greeting="Welcome";
 
-        if(test3.equalsIgnoreCase("Default Welcome Intent")){
+       /* if(test3.equalsIgnoreCase("Default Welcome Intent")){
 
             GregorianCalendar time = new GregorianCalendar();
             if(time.get(Calendar.HOUR_OF_DAY)<12){
@@ -41,7 +43,7 @@ public class HelloWorldController {
                 greeting="Good Evening !";
             }
             greeting=greeting+ ", Welcome to Mobitel Virtual Private Assistant. How Can I help You.";
-        }
+        }*/
         return new WebhookResponse( greeting , "Text " );
     }
 }
