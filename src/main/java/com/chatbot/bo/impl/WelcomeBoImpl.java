5580bd41c1636e9fba8e116668605a739343ca94
sewwandiwi <sewwandisachithra@gmail.com>
2018-06-25 09:22:59 +0530

    webhook catch up

---
+++ src/main/java/com/chatbot/bo/impl/WelcomeBoImpl.java
@@ -0,0 +1,45 @@
+package com.chatbot.bo.impl;
+
+import com.chatbot.bo.FbGraphApiBo;
+import com.chatbot.bo.WelcomeBo;
+import com.chatbot.rest.tx.FbGraphApiUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by sewwandiwi on 6/24/2018.
+ */
+@Service("welcomeBo")
+public class WelcomeBoImpl implements WelcomeBo {
+
+    @Autowired
+    FbGraphApiBo fbGraphApiBo;
+
+    @Override
+    public String greetingText(String senderid) throws IOException {
+        String message="Hi ";
+
+        FbGraphApiUser fbGraphApiUser=fbGraphApiBo.getFbUserName(senderid);
+        String sender=fbGraphApiUser.getFirst_name();
+            //System.out.println("Sender:" +sender);
+        if(fbGraphApiUser.getFirst_name()!=null){
+            message=message +fbGraphApiUser.getFirst_name() +",";
+        }
+
+        GregorianCalendar time = new GregorianCalendar();
+
+        if(time.get(Calendar.HOUR_OF_DAY)<12){
+            message=message+"Good Morning ! ";
+        }else if(time.get(Calendar.HOUR_OF_DAY)>=12 && time.get(Calendar.HOUR_OF_DAY)<16){
+            message=message+"Good Afternoon ! ";
+        }else {
+            message=message+"Good Evening ! ";
+        }
+
+        return message;
+    }
+}
